5f1ff1f0f8a8d9abafd840fb37b193189c6f43d7
Melisa <Melisa@DESKTOP-BOKF80S.fritz.box>
2024-02-22 20:10:40 +0100

    added the rest of the code with some comments for better undertanding

---
+++ src/MainClass.java
@@ -0,0 +1,33 @@
+import org.eclipse.emf.henshin.model.Module;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.model.Unit;
+
+public class MainClass {
+
+    private static final String BASEDIR = "C:\\Users\\Melisa\\eclipse-workspace\\Henshin-bank-example\\org.henshin.bank\\src\\org\\henshin\\bank\\bank.henshin";
+    //private static final String BASEDIR = "C:\\Users\\Melisa\\Desktop\\examples\\diningphils\\diningphils.henshin";
+    //private static final String BASEDIR = "C:\\Users\\Melisa\\eclipse-workspace\\refactoring\\refactorings.henshin";
+    
+    public static void main(String[] args) {
+    	
+        ModuleLoader moduleLoader = new ModuleLoader("");
+
+        Module module = moduleLoader.loadHenshinModule(BASEDIR);
+        if (module != null) {
+            for (Unit unit : module.getUnits()) {
+                if (unit instanceof Rule) {
+                    Rule rule = (Rule) unit;
+                    //System.out.println("Found Rule: " + rule.getName());
+                    // Assign node names using NamingNullNodes
+                    NamingNullNodes nodeNameAssigner = new NamingNullNodes();
+                    nodeNameAssigner.assignNodeNames(rule);;
+
+                }
+            }
+            
+            System.out.println("\n");
+            HenshinRuleParser.parseRules(module);
+   
+        }
+    }
+}
---
+++ src/ModuleLoader.java
@@ -0,0 +1,43 @@
+import org.eclipse.emf.henshin.model.Module;
+import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ModuleLoader {
+
+	private final HenshinResourceSet resourceSet;
+    private static final Logger logger = Logger.getLogger(ModuleLoader.class.getName());
+
+    public ModuleLoader(String workingDirectory) {
+        resourceSet = new HenshinResourceSet(workingDirectory);
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("henshin", new XMIResourceFactoryImpl());
+    }
+
+    public Module loadHenshinModule(String henshinModulePath) {
+        try {
+            Module module = resourceSet.getModule(henshinModulePath);
+            if (module != null) {
+                logger.log(Level.INFO, "Henshin Module loaded successfully: " + henshinModulePath);
+                return module;
+            } else {
+                logger.log(Level.WARNING, "Failed to load the Henshin Module from: " + henshinModulePath);
+                return null;
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error loading Henshin Module: " + e.getMessage(), e);
+            return null;
+        }
+    }
+
+    public void saveHenshinModule(Module module, String outputPath) {
+        try {
+            module.eResource().save(null);
+            logger.log(Level.INFO, "Henshin Module with the new changes saved successfully to: " + outputPath);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error saving Henshin Module: " + e.getMessage(), e);
+        }
+    }
+}
